4751a69feece368eea5e0da73227e11f1615a3dc
Velial72 <abramov1991a@gmail.com>
2023-07-08 09:52:19 +0300

    1

--- main.py
+++ main.py
@@ -4,33 +4,38 @@ import random
 from dotenv import load_dotenv
 
 
-def get_comics_api():
-    global comment
+def get_comics_quantity():
+    url = f'https://xkcd.com/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    comics_quantity = response.json()['num']
+    return comics_quantity
+
+
+def get_comics():
     url = f'https://xkcd.com/{number}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
-    api_comics = response.json()['img']
-    comment = response.json()['alt']
-    save_image(api_comics)
+    random_comics = response.json()
+    comics_url = random_comics['img']
+    comment = random_comics['alt']
+    return comics_url, comment
 
 
 def save_image(url):
     response = requests.get(url)
+    response.raise_for_status()
     with open(filename, 'wb') as file:
         file.write(response.content)
 
 
-def delete_file():
-    os.remove('comics.png')
-
-
-def public_pic(picture, group_id, token):
-    pic_params = f"photo{picture['owner_id']}_{picture['id']}"
+def publish_pic(owner_id, picture_id, group_id, token):
+    picture = f"photo{owner_id}_{picture_id}"
     payload = {
         'access_token': token,
         'owner_id': f'-{group_id}',
         'message': comment,
-        'attachments': pic_params,
+        'attachments': picture,
         'v': 5.131
     }
     url = 'https://api.vk.com/method/wall.post'
@@ -38,12 +43,12 @@ def public_pic(picture, group_id, token):
     response.raise_for_status()
 
 
-def save_pic_to_album(pic, group_id, token):
+def save_pic_to_album(photo_server, photo, photo_hash, group_id, token):
     payload = {
         'group_id': group_id,
-        'server': pic['server'],
-        'photo': pic['photo'],
-        'hash': pic['hash'],
+        'server': photo_server,
+        'photo': photo,
+        'hash': photo_hash,
         'access_token': token,
         'v': 5.131
     }
@@ -51,7 +56,9 @@ def save_pic_to_album(pic, group_id, token):
     response = requests.post(url, params=payload)
     response.raise_for_status()
     params_of_picture = response.json()['response'][0]
-    public_pic(params_of_picture, GROUP_ID, VK_TOKEN)
+    owner_id = params_of_picture['owner_id']
+    picture_id = params_of_picture['id']
+    return owner_id, picture_id
 
 
 def send_pic_to_serv(url, token, group_id):
@@ -62,12 +69,15 @@ def send_pic_to_serv(url, token, group_id):
             'photo': file
         }
         response = requests.post(url, files=files)
-        response.raise_for_status()
+    response.raise_for_status()
     photo_to_save = response.json()
-    save_pic_to_album(photo_to_save, GROUP_ID, VK_TOKEN)
+    photo_server = photo_to_save['server']
+    photo = photo_to_save['photo']
+    photo_hash = photo_to_save['hash']
+    return photo_server, photo, photo_hash
 
 
-def get_server(group_id, token):
+def get_server_url(group_id, token):
     payload = {
         'group_id': group_id,
         'access_token': token,
@@ -77,17 +87,24 @@ def get_server(group_id, token):
     response = requests.get(url, params=payload)
     response.raise_for_status()
     upload_server = response.json()['response']['upload_url']
-    send_pic_to_serv(upload_server, GROUP_ID, VK_TOKEN)
+    return upload_server
 
 
 if __name__ == '__main__':
     load_dotenv()
-    number = random.randint(1, 1000)
+    number = random.randint(1, get_comics_quantity())
     filename = 'comics.png'
-    comment = ''
-    VK_TOKEN = os.getenv("VK_TOKEN")
-    GROUP_ID = os.getenv("VK_GROUP_ID")
+    vk_token = os.getenv("VK_TOKEN")
+    group_id = os.getenv("VK_GROUP_ID")
 
-    get_comics_api()
-    get_server(GROUP_ID, VK_TOKEN)
-    delete_file()
+    comics_url, comment = get_comics()
+    save_image(comics_url)
+    try:
+        upload_server = get_server_url(group_id, vk_token)
+        photo_to_save = send_pic_to_serv(upload_server, group_id, vk_token)
+        params_of_picture = save_pic_to_album(*photo_to_save, group_id, vk_token)
+        publish_pic(*params_of_picture, group_id, vk_token)
+    except ValueError:
+        print("Environment variables error!")
+    finally:
+        os.remove('comics.png')
